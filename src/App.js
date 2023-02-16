1e6beaea7f549c30db9534cebbc0d1ab34268509
stephaniepuertas <puertasstephanie@gmail.com>
2023-02-15 18:48:27 -0800

    adding home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path='/' exact component={Home} />
         </Routes>
       </Router>
     </>
