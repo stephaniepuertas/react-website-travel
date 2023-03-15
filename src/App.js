56f9e144201ac809a2df4cde8e5c521e5c564d71
stephaniepuertas <puertasstephanie@gmail.com>
2023-03-15 14:11:22 -0700

    added video background for main page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact component={Home} />
+          <Route exact path='/' element={<Home/>} />
         </Routes>
       </Router>
     </>
